0eb53d6d22a6b0e8ce85e8f8292dfa061d3cbf85
Anand Vishwakarma <rd.anandv@gmail.com>
2023-07-10 21:33:43 +0530

    rot chiper

---
+++ 34_common_modern_use_of_the_ROT_chiper_where_the_value_of_the_letters_is_shifted_by_A_to_N/dynamic_chiper.js
@@ -0,0 +1,112 @@
+// Function to encrypt a string using ROT13 encryption with a given rotation
+function encryptRotChiper(str, rotation) {
+  var startWord = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  var reverseWord = startWord.substring(rotation) + startWord.substring(0, rotation);
+  var reversed = '';
+
+  // Loop through each character in the input string
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i];
+    var index = -1;
+
+    var isUpperCase = char === char.toUpperCase();
+    var isLowerCase = char === char.toLowerCase();
+
+    // Loop through the startWord to find the index of the character
+    for (var j = 0; j < startWord.length; j++) {
+      if (startWord[j] === char) {
+        index = j;
+        break;
+      }
+    }
+
+    // If the character is found, add the corresponding rotated character from reverseWord
+    if (index > -1) {
+      var rotatedChar = reverseWord[index];
+
+      if (isUpperCase) {
+        rotatedChar = rotatedChar.toUpperCase();
+      } else if (isLowerCase) {
+        rotatedChar = rotatedChar.toLowerCase();
+      }
+
+      reversed += rotatedChar;
+    } else {
+      // If the character is not found, keep it as it is
+      reversed += str[i];
+    }
+  }
+
+  return reversed; // Return the encrypted string
+}
+
+// Input string to be encrypted
+var normalWord = 'My name is Anil yadav number is 1122112';
+
+// Rotation value for encryption
+var rotation = 13;
+
+// Call the encryptRotChiper function to encrypt the input string
+var changedWord = encryptRotChiper(normalWord, rotation);
+
+// Print the encrypted string to the console
+console.log('Encrypted word is: ', changedWord);
+
+
+
+// Function to decrypt a string encrypted with ROT13 encryption with a given rotation
+function decryptRotChiper(rotatedWord, rotation) {
+  var startWord = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+  // Create the reverseWord by rotating the startWord
+  var reverseWord = startWord.substring(rotation) + startWord.substring(0, rotation);
+
+  // Rotate the startWord again to get the original startWord
+  var againReverseWord = startWord.substring(rotation) + startWord.substring(0, rotation);
+
+  var reversed = '';
+
+  // Loop through each character in the rotatedWord
+  for (var i = 0; i < rotatedWord.length; i++) {
+    var char = rotatedWord[i];
+    var index = -1;
+
+    var isUpperCase = char === char.toUpperCase();
+    var isLowerCase = char === char.toLowerCase();
+
+    // Loop through the startWord to find the index of the character
+    for (var j = 0; j < startWord.length; j++) {
+      if (startWord[j] === char) {
+        index = j;
+        break;
+      }
+    }
+
+    // If the character is found, add the corresponding character from againReverseWord
+    if (index > -1) {
+      var rotatedChar = againReverseWord[index];
+
+      if (isUpperCase) {
+        rotatedChar = rotatedChar.toUpperCase();
+      } else if (isLowerCase) {
+        rotatedChar = rotatedChar.toLowerCase();
+      }
+
+      reversed += rotatedChar;
+    } else {
+      // If the character is not found, keep it as it is
+      reversed += rotatedWord[i];
+    }
+  }
+
+  return reversed; // Return the decrypted string
+}
+
+// Input string to be decrypted
+var reverseWord = 'Zl anzr vf Navy lnqni ahzore vf 1122112';
+
+// Call the decryptRotChiper function to decrypt the input string
+var result = decryptRotChiper(reverseWord, rotation);
+
+// Print the decrypted string to the console
+console.log('Decrypted word is: ', result);
